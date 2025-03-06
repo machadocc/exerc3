379d8335f90d07b5ee625114d01591066c6571a2
machadoc <cristhianmac123@gmail.com>
2025-03-06 19:56:22 -0300

    Primeiro commit

--- index.php
+++ index.php
@@ -8,9 +8,8 @@ $stylesheet = file_get_contents('style.css');
 $mpdf->WriteHTML($stylesheet, \Mpdf\HTMLParserMode::HEADER_CSS);
 
 $html = '
-    <h1>Atenção!!!!!</h1>
-    <p>Melhor camisa 10 em atividade no futebol brasileiro abaixo</p>
-    <img src="https://www.estadao.com.br/resizer/v2/GCO4W72LJJFG7PYCXH65MQFRDM.jpg?quality=80&auth=884b3c5b55121676f3136ca1322143a807dea12dd1bc40ca5e44c1dbf49c1f2f&width=380">
+    <h1>Atenção</h1>
+    <p>Ola mundo</p>
 ';
 
 $mpdf->WriteHTML($html);
